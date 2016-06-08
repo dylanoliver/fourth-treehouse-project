cc576cd1ec6331e912940a3ca39a8ca8b531d766
dylanoliver <dylanoliver@me.com>
2016-06-08 08:58:49 +0000

    Add files via upload

---
+++ js/filter-search.js
@@ -0,0 +1,32 @@
+(function() {
+	var $imgs = $('#imageGallery img'); //Get the images
+	var $search = $('#filter-search');  //Get the input element
+	var cache = [];						//Create an array called cache
+
+	$imgs.each(function() {				//For each image
+		cache.push({					//Add an object to the cache array
+			element: this,				//This image
+			text: this.alt.trim().toLowerCase()		//Its alt text (lowercase trimmed)
+		});
+	});
+
+	function filter() {
+		var query = this.value.trim().toLowerCase();
+
+		cache.forEach(function(img) {
+			var index = 0;
+			if (query) {
+				index = img.text.indexOf(query);
+			}
+
+			img.element.style.display = index === -1 ? 'none' : '';
+		});
+	}
+
+	if ('oninput' in $search[0]) {
+		$search.on('input', filter);
+	} else {
+		$search.on('keyup', filter);
+	}
+}());
+
